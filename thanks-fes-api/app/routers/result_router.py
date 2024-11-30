b6ee12a38a01d28305834599f366cd9689fdd8f8
takanobu <Takanobu.Ohnuma@ibm.com>
2024-12-01 06:09:30 +0900

    finalize

--- thanks-fes-api/app/routers/result_router.py
+++ thanks-fes-api/app/routers/result_router.py
@@ -2,22 +2,23 @@ from fastapi import APIRouter
 
 from app.cruds import answer_crud, panelist_crud
 from app.models.result_model import ResultModel
+from app.utils.http_error_util import raise_http_exception
 
 router = APIRouter()
 
 
 def fetch_panelist_results(period: int = None) -> list[ResultModel]:
     results = []
-    rank = _total_score = _elapsed_second = 0
+    rank = _score = _elapsed_second = 0
     _results = answer_crud.get_panelist_results() if period is None \
             else answer_crud.get_panelist_period_results(period)
-    for _id, total_score, elapsed_second in _results:
-        if _total_score != total_score or _elapsed_second != elapsed_second:
+    for _id, score, elapsed_second in _results:
+        if _score != score or _elapsed_second != elapsed_second:
             rank += 1
-            _total_score, _elapsed_second = total_score, elapsed_second
+            _score, _elapsed_second = score, elapsed_second
         result = ResultModel(
             name=panelist_crud.get_name(_id),
-            score=total_score,
+            score=score,
             elapsed_second=elapsed_second,
             rank=rank
         )
@@ -44,10 +45,20 @@ def fetch_team_results(period: int = None) -> list[ResultModel]:
     return results
 
 
+dummy_result = ResultModel(
+            name='',
+            score=0,
+            elapsed_second=0,
+            rank=0
+        )
+
 @router.get('/panelists/{panelist_id}', summary='個人総合結果取得API', response_model=ResultModel)
 async def get_panelist_result(panelist_id: int):
-    name = panelist_crud.get_name(panelist_id)
-    return [x for x in fetch_panelist_results() if x.name == name][0]
+    panelist = panelist_crud.get(panelist_id)
+    if panelist is None:
+        raise_http_exception(404, 'panelist not found')
+    results = [x for x in fetch_panelist_results() if x.name == panelist.name]
+    return results[0] if results else dummy_result
 
 
 @router.get('/panelists', summary='個人総合結果リスト取得API', response_model=list[ResultModel])
@@ -57,7 +68,11 @@ async def get_panelist_results():
 
 @router.get('/teams/{team}', summary='チーム総合結果取得API', response_model=ResultModel)
 async def get_team_result(team: str):
-    return [x for x in fetch_team_results() if x.name == team][0]
+    teams = panelist_crud.get_teams()
+    if team not in teams:
+        raise_http_exception(404, 'team not found')
+    results = [x for x in fetch_team_results() if x.name == team]
+    return results[0] if results else dummy_result
 
 
 @router.get('/teams', summary='チーム総合結果リスト取得API', response_model=list[ResultModel])
@@ -66,9 +81,12 @@ async def get_team_results():
 
 
 @router.get('/panelists/{panelist_id}/periods/{period}', summary='個人ピリオド結果取得API', response_model=ResultModel)
-async def get_panelist_period_result(period: int, panelist_id: int):
-    name = panelist_crud.get_name(panelist_id)
-    return [x for x in fetch_panelist_results(period) if x.name == name][0]
+async def get_panelist_period_result(panelist_id: int, period: int):
+    panelist = panelist_crud.get(panelist_id)
+    if panelist is None:
+        raise_http_exception(404, 'panelist not found')
+    results = [x for x in fetch_panelist_results(period) if x.name == panelist.name]
+    return results[0] if results else dummy_result
 
 
 @router.get('/panelists/periods/{period}', summary='個人ピリオド結果リスト取得API', response_model=list[ResultModel])
@@ -78,7 +96,11 @@ async def get_panelist_periods_result(period: int):
 
 @router.get('/teams/{team}/periods/{period}', summary='チームピリオド結果取得API', response_model=ResultModel)
 async def get_team_period_result(team: str, period: int):
-    return [x for x in fetch_team_results(period) if x.name == team][0]
+    teams = panelist_crud.get_teams()
+    if team not in teams:
+        raise_http_exception(404, 'team not found')
+    results = [x for x in fetch_team_results(period) if x.name == team]
+    return results[0] if results else dummy_result
 
 
 @router.get('/teams/periods/{period}', summary='チームピリオド結果リスト取得API', response_model=list[ResultModel])
