520391dcf7b1234f424e8a6ba9366fd114d54f31
takanobu <Takanobu.Ohnuma@ibm.com>
2025-07-05 13:21:46 +0900

    useHash: false

--- thanks-fes-web/src/app/app-routing.module.ts
+++ thanks-fes-web/src/app/app-routing.module.ts
@@ -31,7 +31,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      useHash: true,
+      useHash: false,
       enableTracing: false,
       onSameUrlNavigation: 'reload',
       preloadingStrategy: AppPreloadingStrategy,
