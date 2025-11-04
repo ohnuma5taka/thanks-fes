6c456813c3b9d01596720ecf2975cab33b0fa1ce
Takanobu Ohnuma <Takanobu.Ohnuma@ibm.com>
2025-11-04 22:21:55 +0900

    restore seed data

--- thanks-fes-api/README.md
+++ thanks-fes-api/README.md
@@ -2,29 +2,35 @@
 
 ## For Production
 
-```
+```bash
 docker build --build-arg APP_MODE=prod -t thanks-fes-api .
 docker network create thanks-fes-network
 docker run --name thanks-fes-api -itd -p 8888:8888 --network thanks-fes-network --volume ./app:/src/app --tty thanks-fes-api
 ```
 
 ### Seed
-```
+
+```bash
 docker exec -it thanks-fes-api python /src/seed.py
 ```
 
 ## For Local
 
+Start Redis
+
+```bash
+redis-server redis.conf
 ```
+
+```bash
 python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-redis-server redis.conf
 python main.py
 ```
 
 ### Seed
 
-```
+```bash
 python seed.py --mode dev
 ```
--- thanks-fes-api/app/db/seeds/team_seed.py
+++ thanks-fes-api/app/db/seeds/team_seed.py
@@ -1,14 +1,13 @@
 import csv
-from app.cruds import option_crud
-from app.db.models.option import Option
+
+from app.cruds import team_crud
+from app.db.models.team import Team
 
 
 def seed_teams(seeds_data_dir: str):
     teams = [
-        Option(
-            name=row['name'],
-            color=row['color']
-        ) for row in csv.DictReader(open(f'{seeds_data_dir}/teams.tsv'), delimiter='\t')
+        Team(name=row["name"], color=row["color"])
+        for row in csv.DictReader(open(f"{seeds_data_dir}/teams.tsv"), delimiter="\t")
     ]
-    option_crud.reset_all(teams)
-    print(f'teams: {len(teams)} records')
+    team_crud.reset_all(teams)
+    print(f"teams: {len(teams)} records")
