cf2a64cc0e19454a0d7809309047816243c2e7e7
Takanobu Ohnuma <Takanobu.Ohnuma@ibm.com>
2026-02-16 03:17:10 +0900

    update README

--- setup.sh
+++ setup.sh
@@ -1,23 +1,26 @@
 #! /bin/bash
-BRANCH_NAME=xxx
+BRANCH_NAME=__BRANCH_NAME__
 sudo yum update
 sudo yum install -y docker git
 sudo systemctl start docker
-sudo gpasswd -aG $(whoami) docker
+sudo usermod -aG docker $(whoami)
 sudo chgrp docker /var/run/docker.sock
 sudo service docker restart
 sudo systemctl enable docker
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.32.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-chmod 400 ~/.ssh/git_id_rsa
-cat << 'EOF' > ~/.ssh/config
+cd /home/ec2-user
+chmod 400 .ssh/git_id_rsa
+set -e
+cat << 'EOF' > .ssh/config
 Host github.com
     HostName github.com
-    IdentityFile ~/.ssh/git_id_rsa
+    IdentityFile /home/ec2-user/.ssh/git_id_rsa
     User git
 EOF
-git clone -b $BRANCH_NAME git@github.com:ohnuma5taka/pikmiverse.git && \
+chmod 600 .ssh/config
+sudo -u ec2-user git clone -b $BRANCH_NAME git@github.com:ohnuma5taka/thanks-fes.git && \
 sudo cp thanks-fes/entrypoint.service /etc/systemd/system/entrypoint.service && \
 sudo systemctl daemon-reexec && \
 sudo systemctl daemon-reload && \
